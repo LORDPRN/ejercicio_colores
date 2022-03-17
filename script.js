ff85dd0d04d2b27b00dc726f0a56787b562a922b
Abdiel Arias <81927109+LORDPRN@users.noreply.github.com>
2022-03-16 22:40:53 -0500

    DOM - COLORES

---
+++ script.js
@@ -0,0 +1,22 @@
+const boton = document.getElementsByClassName('boton');
+const colores = ["#ff6961","#77dd77","#fdfd96","#84b6f4","#fdcae1",
+                 "#fa34df","#7ff9c7","#b186f1","#a68069","#eb9cff"];
+
+for (let index = 0; index < boton.length; index++) {
+    const element = boton[index];
+    const element1 = container[index]
+    element.addEventListener("click", cambiarFondo);
+}
+
+function cambiarFondo(button){
+    const indice = Math.floor((Math.random() * colores.length));
+    const contenedor = this.parentNode;
+    //console.log(contenedor);
+    contenedor.style.backgroundColor = colores[indice];
+    
+    const texto = this.parentNode.childNodes[1];
+    texto.innerHTML = colores[indice];
+
+    //console.log(texto)
+    //console.log(this);
+}
